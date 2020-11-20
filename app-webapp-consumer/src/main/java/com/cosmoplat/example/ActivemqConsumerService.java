ba2a536692a4760ba900ae57ffa50771886c0d2a
jiangxiaohong@haier.com <haier20191025>
2020-11-20 14:11:59 +0800

    注释

--- app-webapp-consumer/src/main/java/com/cosmoplat/example/ActivemqConsumerService.java
+++ app-webapp-consumer/src/main/java/com/cosmoplat/example/ActivemqConsumerService.java
@@ -20,7 +20,6 @@ public class ActivemqConsumerService {
     /**
      * 使用JmsListener配置消费者监听的队列，其中name是接收到的消息
       */
-    // 使用JmsListener配置消费者监听的队列
     @JmsListener(destination = "test")
     public void handleMessage(String msg) {
         log.info("服务端成功接收queue消息成功：{}",msg);
